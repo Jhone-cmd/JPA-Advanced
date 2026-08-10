dc515d159f507ae546fd661ef28e5f11a162e496
Jhone-cmd <jhone.1941@gmail.com>
2026-08-10 15:12:30 -0300

    initial commit

---
+++ src/test/java/com/jhonecmd/jpa/advanced/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonecmd.jpa.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
