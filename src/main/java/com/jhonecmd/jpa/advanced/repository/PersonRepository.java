af76c90c3853c35cd217697847d533583457ee25
Jhone-cmd <jhone.1941@gmail.com>
2026-08-12 16:25:21 -0300

    add page person service

--- src/main/java/com/jhonecmd/jpa/advanced/repository/PersonRepository.java
+++ src/main/java/com/jhonecmd/jpa/advanced/repository/PersonRepository.java
@@ -1,10 +1,14 @@
 package com.jhonecmd.jpa.advanced.repository;
 
 import com.jhonecmd.jpa.advanced.model.PersonEntity;
+import org.hibernate.query.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.awt.print.Pageable;
 import java.util.UUID;
 
 public interface PersonRepository extends JpaRepository<PersonEntity, UUID>, JpaSpecificationExecutor<PersonEntity> {
+    Page findAll(Specification<PersonEntity> persons, Pageable pageable);
 }
--- src/main/java/com/jhonecmd/jpa/advanced/service/PersonService.java
+++ src/main/java/com/jhonecmd/jpa/advanced/service/PersonService.java
@@ -1,10 +1,14 @@
 package com.jhonecmd.jpa.advanced.service;
 
+import com.jhonecmd.jpa.advanced.dto.PersonFilterDTO;
 import com.jhonecmd.jpa.advanced.model.PersonEntity;
 import com.jhonecmd.jpa.advanced.repository.PersonRepository;
+import com.jhonecmd.jpa.advanced.specification.PersonSpecification;
 import lombok.AllArgsConstructor;
+import org.hibernate.query.Page;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
@@ -12,8 +16,13 @@ import java.util.List;
 public class PersonService {
 
     private final PersonRepository personRepository;
+    private final PersonSpecification personSpecification;
 
     public List<PersonEntity> execute() {
-        return  personRepository.findAll();
+        return personRepository.findAll();
+    }
+
+    public Page<PersonEntity> execute(PersonFilterDTO filter, Pageable pageable) {
+        return personRepository.findAll(personSpecification.persons(filter), pageable);
     }
 }
